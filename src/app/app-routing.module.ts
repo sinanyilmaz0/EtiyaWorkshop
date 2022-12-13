c5889e6f7628eec10180187eaa3a3ffc40d4b05a
sinanyilmaz0 <sinan.yilmaz197@gmail.com>
2022-12-13 15:48:05 +0300

    modul structure updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { CategoryListComponent } from './features/category/components/category-l
 import { CategoryListPageComponent } from './features/category/pages/category-list-page/category-list-page.component';
 import { DashboardCategoriesPageComponent } from './features/category/pages/dashboard-categories-page/dashboard-categories-page.component';
 import { DashboardProductsPageComponent } from './features/product/pages/dashboard-products-page/dashboard-products-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { LoginPageComponent } from './shared/pages/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { ProductFormPageComponent } from './features/product/pages/product-form-page/product-form-page.component';
 
